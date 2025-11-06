2f9804d2f8484a4ee4d1f7f9f7465057f5545fcc
firestige <zzt.nbl@hotmail.com>
2025-11-06 22:28:08 +0800

    添加测试内容

--- web-app/src/pages/LibraryPage.tsx
+++ web-app/src/pages/LibraryPage.tsx
@@ -1,14 +1,81 @@
 import * as React from 'react';
 import LibraryToolbar from '../components/toolbar/LibraryToolbar.tsx';
-import { useAssetsLibrary } from '../features/library/hooks/useAssetsLibrary.ts';
+
+const example20 = (
+  <div className="flex flex-col">
+    <div>
+      <h3>20型材</h3>
+    </div>
+    <div className="flex flex-row flex-wrap gap-3">
+      <div className="flex flex-col">
+        <svg className="bg-amber-400 w-40 h-32" />
+        <span>2020</span>
+      </div>
+      <div className="flex flex-col">
+        <svg className="bg-amber-400 w-40 h-32" />
+        <span>2020L</span>
+      </div>
+      <div className="flex flex-col">
+        <svg className="bg-amber-400 w-40 h-32" />
+        <span>2020W</span>
+      </div>
+      <div className="flex flex-col">
+        <svg className="bg-amber-400 w-40 h-32" />
+        <span>2040</span>
+      </div>
+      <div className="flex flex-col">
+        <svg className="bg-amber-400 w-40 h-32" />
+        <span>2060</span>
+      </div>
+      <div className="flex flex-col">
+        <svg className="bg-amber-400 w-40 h-32" />
+        <span>2020LE</span>
+      </div>
+    </div>
+  </div>
+);
+const example30 = (
+  <div className="flex flex-col">
+    <div>
+      <h3>30型材</h3>
+    </div>
+    <div className="flex flex-row flex-wrap gap-3">
+      <div className="flex flex-col">
+        <svg className="bg-amber-400 w-40 h-32" />
+        <span>3030</span>
+      </div>
+      <div className="flex flex-col">
+        <svg className="bg-amber-400 w-40 h-32" />
+        <span>3030L</span>
+      </div>
+      <div className="flex flex-col">
+        <svg className="bg-amber-400 w-40 h-32" />
+        <span>3030W</span>
+      </div>
+      <div className="flex flex-col">
+        <svg className="bg-amber-400 w-40 h-32" />
+        <span>3040</span>
+      </div>
+      <div className="flex flex-col">
+        <svg className="bg-amber-400 w-40 h-32" />
+        <span>3060</span>
+      </div>
+      <div className="flex flex-col">
+        <svg className="bg-amber-400 w-40 h-32" />
+        <span>3030LE</span>
+      </div>
+    </div>
+  </div>
+);
 
 const LibraryPage: React.FC = () => {
-  const {assets, add, update, remove} = useAssetsLibrary();
   return (
     <div className="library-page p-4 ">
       <LibraryToolbar />
-      <div className="page-content">
-        {assets.map((asset) => )}
+      <div className="page-content flex flex-col gap-3">
+        {example20}
+        <div className="border-1" />
+        {example30}
       </div>
     </div>
   );
